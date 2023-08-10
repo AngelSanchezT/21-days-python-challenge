f53a03a08b87d5bb2288418dc1f9c0f75cdc1e55
AngelSanchezT <ajsanchez22@gmail.com>
2023-08-10 02:45:45 +0000

    ✅ Day-02: Calculate Tip

--- day_02/exercise_02.py
+++ day_02/exercise_02.py
@@ -1,3 +1,6 @@
-def calculate_tip(bill_amount, tipPercentage):
-   # Tu código aquí 👈
-   pass
+def calculate_tip(bill_amount, tip_percentage):
+  tip_value = (bill_amount * tip_percentage) / 100
+  return round(tip_value, 2)
+
+print(calculate_tip(100, 10))
+print(calculate_tip(1524.33, 25))
