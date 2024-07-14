c16a19da121e778ae720a86c3c68a3ae74dacc53
Angel Javier Sanchez <ajsanchez22@gmail.com>
2024-07-13 22:54:12 -0500

    🚧 In Progress Exercise 05

--- day_04/exercise_05.py
+++ day_04/exercise_05.py
@@ -1,2 +1,13 @@
 def find_famous_cat(cats):
-  pass
+    max_followers = 0
+    famous_cats = []
+    
+    for cat in cats:
+        total_followers = sum(cat["followers"])
+        if total_followers > max_followers:
+            max_followers = total_followers
+            famous_cats = [cat["name"]]
+        elif total_followers == max_followers:
+            famous_cats.append(cat["name"])
+    
+    return famous_cats
