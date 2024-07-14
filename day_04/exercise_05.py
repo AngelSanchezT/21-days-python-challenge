7de95b9e086c155ddf316b1e0e41fa2a3e88be8f
Angel Javier Sanchez <ajsanchez22@gmail.com>
2024-07-13 23:02:29 -0500

    🚧 Exercise 05

--- day_04/exercise_05.py
+++ day_04/exercise_05.py
@@ -11,3 +11,30 @@ def find_famous_cat(cats):
             famous_cats.append(cat["name"])
     
     return famous_cats
+
+# Ejemplo de uso
+print(find_famous_cat([
+    {
+        "name": "Luna",
+        "followers": [500, 200, 300]
+    },
+    {
+        "name": "Michi",
+        "followers": [100, 300]
+    },
+]))
+
+print(find_famous_cat([
+    {
+        "name": "Mimi",
+        "followers": [320, 120, 70]
+    },
+    {
+        "name": "Milo",
+        "followers": [400, 300, 100, 200]
+    },
+    {
+        "name": "Gizmo",
+        "followers": [250, 750]
+    }
+]))
